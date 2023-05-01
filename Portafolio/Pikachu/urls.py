89ddde744834d64088b5642b911ecca0ab07c013
Axel José Mosquito <112713383+AxlRoose1702@users.noreply.github.com>
2023-05-01 15:48:13 -0600

    [ADD]: se agrega modelo y nueva configuracion

--- Portafolio/Pikachu/urls.py
+++ Portafolio/Pikachu/urls.py
@@ -3,10 +3,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index, name="inicio"),
-    path('donwload', views.descargacv, name="descargarcv"),
-    path('exp', views.exp, name="exp"),
-    path('about', views.about, name="about"),
-    path('project', views.project, name="proyectos"),
+    path('', views.home, name="inicio"),
     
 ]
